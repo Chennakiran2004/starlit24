1955af966f8f6c6d475417e3ac9174e8640f80a3
PoojithBommana <nanipoojith03@gmail.com>
2024-12-09 23:09:07 +0530

    added head tag

--- src/components/activities/leaderboard.tsx
+++ src/components/activities/leaderboard.tsx
@@ -18,19 +18,19 @@ interface ClassStanding {
 
 const LeaderboardLineChart: React.FC = () => {
   const [standings, setStandings] = useState<ClassStanding[]>([
-    { className: "s1", points: 0 },
-    { className: "s2", points: 0 },
+    { className: "s1", points: 20 },
+    { className: "s2", points: 30 },
     { className: "s3", points: 0 },
-    { className: "s4", points: 0 },
+    { className: "s4", points: 20 },
     { className: "s5", points: 0 },
-    { className: "s6", points: 0 },
-    { className: "s7", points: 0 },
+    { className: "s6", points: 20 },
+    { className: "s7", points: 30 },
     { className: "s8", points: 0 },
-    { className: "s9", points: 0 },
-    { className: "s10", points: 0 },
-    { className: "s11", points: 0 },
-    { className: "s12", points: 0 },
-    { className: "s13", points: 0 },
+    { className: "s9", points: 50 },
+    { className: "s10", points: 30 },
+    { className: "s11", points: 50 },
+    { className: "s12", points: 20 },
+    { className: "s13", points: 30 },
     { className: "s14", points: 0 },
   ]);
 
